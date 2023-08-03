0309f206ee9979b961258e49c03f62d5dc547047
Jaakko Hannikainen <jgke@jgke.fi>
2023-08-03 13:31:33 +0300

    Add ball example

--- GameImpl/Game1.cs
+++ GameImpl/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,19 +7,28 @@ namespace MyGame;
 
 public class Game1 : Game
 {
+    Texture2D ballTexture;
+    Vector2 ballPosition;
+    float ballSpeed;
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    public readonly int PhysicsFps;
 
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
+        this.PhysicsFps = 5; // Change this to something sensible later
+        TargetElapsedTime = TimeSpan.FromSeconds(1f / PhysicsFps);
+        IsFixedTimeStep = true;
     }
 
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
+        ballSpeed = 100f;
 
         base.Initialize();
     }
@@ -28,16 +38,60 @@ public class Game1 : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
+        ballTexture = Content.Load<Texture2D>("ball");
     }
 
-    protected override void Update(GameTime gameTime)
+    protected override void FixedUpdate(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
+        var kstate = Keyboard.GetState();
 
-        base.Update(gameTime);
+        if (kstate.IsKeyDown(Keys.Up))
+        {
+            ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+
+        if(kstate.IsKeyDown(Keys.Down))
+        {
+            ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+
+        if (kstate.IsKeyDown(Keys.Left))
+        {
+            ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+
+        if(kstate.IsKeyDown(Keys.Right))
+        {
+            ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+
+        if(ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2)
+        {
+            ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
+        }
+        else if(ballPosition.X < ballTexture.Width / 2)
+        {
+            ballPosition.X = ballTexture.Width / 2;
+        }
+
+        if(ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
+        {
+            ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
+        }
+        else if(ballPosition.Y < ballTexture.Height / 2)
+        {
+            ballPosition.Y = ballTexture.Height / 2;
+        }
+
+        base.FixedUpdate(gameTime);
+    }
+
+    protected override void DrawUpdate(GameTime gameTime)
+    {
+        base.DrawUpdate(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
@@ -45,6 +99,19 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(
+            ballTexture,
+            ballPosition,
+            null,
+            Color.White,
+            0f,
+            new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+            Vector2.One,
+            SpriteEffects.None,
+            0f
+        );
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
